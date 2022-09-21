d866919fb2b2eb59a69389ab11045f82976b6d58
Pedro Magno <108394410+Pedro-Magno@users.noreply.github.com>
2022-09-21 20:40:39 -0300

    CSV almost, need to manually check

--- Web page to csv.py
+++ Web page to csv.py
@@ -10,13 +10,13 @@ page = urllib.request.Request("https://en.wikipedia.org/wiki/Computer_science")
 page.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
 
 
-#Abre a página e transforma tudo em um código html legível em utf-8
+#Abre a página e transforma tudo em um código html utf-8
 page = urllib.request.urlopen(page)
 page = page.read().decode("utf-8")
 page = BeautifulSoup(page, "html.parser")
 
 
-#Seleciona a div que possui todo o conteúdo da página e salva o conteudo numa lista, no caso da Wikipedia todo o conteúdo se encontra na div de classe "mw-parser-output"
+#Seleciona a div que possui o conteúdo da página e salva o conteudo numa lista, no caso da Wikipedia o conteúdo se encontra na div de classe "mw-parser-output"
 list = []
 for x in page.find_all("div", class_="mw-parser-output"):
     list.append(x.text)
--- most-common-words-1k/100 most common remover.py
+++ most-common-words-1k/100 most common remover.py
@@ -1,3 +1,20 @@
 import pandas
 
-pandas.
+Removal_DF = pandas.read_csv("most-common-words-1k.csv")
+Removal_DF = Removal_DF.drop(Removal_DF[Removal_DF["Main Type"] == "noun"].index)
+Word_Collumn1 = Removal_DF.loc[:,"Word"]
+List1 = Word_Collumn1.values.tolist()
+List1_Upper = []
+for i in List1:
+    List1_Upper.append(i.upper())
+To_Remove_DF = pandas.read_csv("../Dataframe.csv")
+Word_Collumn2 = To_Remove_DF.loc[:,"Word"]
+List2 = Word_Collumn2.values.tolist()
+
+for i in List1_Upper:
+        To_Remove_DF = To_Remove_DF.drop(To_Remove_DF[To_Remove_DF["Word"] == i].index)
+#To_Remove_DF = To_Remove_DF.drop(To_Remove_DF.columns[0], axis = 1)
+To_Remove_DF.to_csv("Semi_Formatted_Dataframe.csv")
+
+print(To_Remove_DF)
+print(List2)
