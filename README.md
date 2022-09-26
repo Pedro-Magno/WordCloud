5c237e30d5fcfe406afa21a2a5f1b2a39eeff819
Pedro Magno <108394410+Pedro-Magno@users.noreply.github.com>
2022-09-25 23:08:00 -0300

    README and English
    Finished README and changed everything to English

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WordCloud
- Program to create a wordcloud out of a web link
+    Python program to create a wordcloud out of a wikipedia web link.
+    To change the aesthetics of the wordcloud , only the WordCloud function should be edited, recommend using the official documentation for that: https://amueller.github.io/word_cloud/references.html
+    Var 'list_stopwords' should be the only thing that needs to be changed with each different wiki page, everything else should work fine.
--- Wordcloud.py
+++ Wordcloud.py
@@ -5,37 +5,37 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud,STOPWORDS
 
 
-#Determina a página selecionada, segunda linha finge ser um browser normal, muitas páginas não aceitam abrir sem um browser-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Which wikipedia page you want to be opened, second line pretends to be a Web browser, many pages don´t open without it-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 page = request.Request("https://en.wikipedia.org/wiki/Computer_science")
 page.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
 
 
-#Abre a página e transforma tudo em um código html utf-8-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Opens the page and transforms everything to an html-utf-8 code-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 page = request.urlopen(page)
 page = page.read().decode("utf-8")
 page = BeautifulSoup(page, "html.parser")
 
 
-#Seleciona a div que possui o conteúdo da página e salva o conteudo numa lista, no caso da Wikipedia o conteúdo se encontra na div de classe "mw-parser-output"-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Selects the div that has the page's content and saves it all into a list, in the case of Wikipedia, the div with all the content is 'mw-parser-output'-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 list = []
 for x in page.find_all("div", class_="mw-parser-output"):
     list.append(x.text)
     list.append(" ")
 
 
-#Formatação do conteúdo da página, remoção de caracteres não letras-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Formmating of the page's content, removes everything that isn´t letters and spaces-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 final_string = "".join(list)
 final_string = final_string.upper()
 final_string = sub(r'[^a-zA-Z\s]', '', final_string)
 
 
-#Stopwords a serem removidos do Wordcloud-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-lista_stopwords = ["D","X","TWO","AUGUST","FORM","BECAME","USED","OCTOBER","FOUND","P","ADA","ISBN","APRIL","SEE","TERM","DENNING","IEEE","BABBAGE","DECEMBER","ONE","THREE","AIM","SCID","WAY","MAY","COMMON","DOI","B","DE","USE","ARGUED","FEBRUARY","CHARLES","ISBN","SEPTEMBER","OFTEN","C","MARCH","JUNE","J","S","NAME"]
-for i in lista_stopwords:
+#Stopwords to be removed from the Wordcloud-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+list_stopwords = ["D","X","TWO","AUGUST","FORM","BECAME","USED","OCTOBER","FOUND","P","ADA","ISBN","APRIL","SEE","TERM","DENNING","IEEE","BABBAGE","DECEMBER","ONE","THREE","AIM","SCID","WAY","MAY","COMMON","DOI","B","DE","USE","ARGUED","FEBRUARY","CHARLES","ISBN","SEPTEMBER","OFTEN","C","MARCH","JUNE","J","S","NAME"]
+for i in list_stopwords:
     STOPWORDS.add(i)
 
 
-#Wordcloud em si-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Wordcloud itself-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 wordcloud = WordCloud(width= 1920, height= 1080, collocations = False, background_color = 'white', colormap='plasma', stopwords=STOPWORDS).generate(final_string)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
